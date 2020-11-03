fa7e65d85902549f7c0488246e1fa308812f1763
Braden Morley <bradenmorley@gmail.com>
2020-11-03 11:33:43 -0500

    Added container to app

--- instrument_tracker/src/App.js
+++ instrument_tracker/src/App.js
@@ -11,12 +11,14 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
       <Router>
+          <div className="container">
           <Navbar />
           <br/>
           <Route path="/" exact component={InstrumentsList} />
           <Route path="/edit/:id" exact component={EditInstrument} />
           <Route path="/create" exact component={CreateInstrument} />
           <Route path="/user" exact component={CreateUser} />
+          </div>
       </Router>
   );
 }
